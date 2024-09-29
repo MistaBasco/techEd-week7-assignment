373218819f2582575b49043e67aa41da527f68f7
MistaBasco <mistabasco>
2024-09-30 00:14:30 +0100

    hopefully the last set of broken links

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -36,12 +36,16 @@ export default function ReviewCard({
 
   return (
     <div className="ReviewCard">
-      <Link to={`/anime/${anime_id}`}>
+      <Link
+        to={`https://teched-week7-assignment.onrender.com/anime/${anime_id}`}
+      >
         <h3>{anime_title}</h3>
       </Link>
       <div className="spacer">
         <strong>Review by:</strong>
-        <Link to={`/user/${user_id}`}>
+        <Link
+          to={`https://teched-week7-assignment.onrender.com/user/${user_id}`}
+        >
           <p>{reviewer_name}</p>
         </Link>
       </div>
